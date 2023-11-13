74fa4387d55e219d77d68fc1dbd1e7db10e708b3
SIMple-lives <1305969480@qq.com>
2023-11-13 21:35:24 +0800

    second

--- c-primer-plus/8.11.c
+++ c-primer-plus/8.11.c
@@ -1,15 +1,39 @@
 #include<stdio.h>
 #include<string.h>
- 
+#include<ctype.h>
+
 int main()
+{
+    char a;
+    int big=0;
+    int small=0;
+    do
+    {
+        a=getchar();
+        if(islower(a))
+        {
+            small++;
+        }
+        else if(isupper(a))
+        {
+            big++;
+        }
+    } while (a!='&');
+    
+    printf("big=%d small=%d\n",big,small);
+    return 0;
+}
+
+
+/* int main()
 {
     char a='0';
     int count=0;
-    while(a=getchar()!=EOF)
+    while(a=getchar()!='&')
     {
         count++;
         getchar();
     }
     printf("%d\n",count); 
     return 0;
-}
+} */
